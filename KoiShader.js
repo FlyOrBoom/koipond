9b7e0a2c228bbe9404d529b5f8ca63b4d90d4655
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2020-12-30 12:13:20 -0800

    Half resolution sampling

--- KoiShader.js
+++ KoiShader.js
@@ -110,7 +110,7 @@ function start () {
 start()
 
 function resize () {
-    resizeCanvasToDisplaySize(gl.canvas, 1)
+    resizeCanvasToDisplaySize(gl.canvas, 0.5) // Half resolution sampling
 }
 window.addEventListener('resize',resize)
 resize()
